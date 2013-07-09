c3e0a0fea081733437ec8230848ab841ed30a516
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2013-07-09 13:35:42 +0200

    Include script that triggers jekyll (cherry picked from commit 8d683f6e51f32cf71f27f526098cb152e9b867ab)
    Remove master specific additions.

---
+++ _scripts/_run_jekyll.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. `dirname "$0"`/functions
+
+export WRKDIR=$WORKDIR
+
+sed '/^\[.*\[.*\].*\]/d' $WRKDIR/documentation-generator/_references.md > $WRKDIR/documentation-generator/new_references.md
+mv $WRKDIR/documentation-generator/new_references.md $WRKDIR/documentation-generator/_references.md
+
+mkdir $WRKDIR/documentation-generator/pages
+cp -r $WRKDIR/documentation/* $WRKDIR/documentation-generator/pages
+cd $WRKDIR/documentation-generator
+source /home/vagrant/.rvm/scripts/rvm
+echo "Latest jekyll run :$BUILD_ID" > $WRKDIR/output.log
+echo "Based on latest git commit :$GIT_COMMIT" >> $WRKDIR/output.log
+echo "*********************************************************" >> $WRKDIR/output.log
+echo "*                  CONSOLE OUTPUT                       *" >> $WRKDIR/output.log
+echo "*********************************************************" >> $WRKDIR/output.log
+jekyll
+$WRKDIR/documentation-generator/_scripts/cfdoc_postprocess.py
