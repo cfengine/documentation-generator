f2fbf21cb053092ab8712c088f3201cca4829c08
Volker Hilsheimer <volker.hilsheimer@gmail.com>
2014-10-11 13:33:31 +0200

    Consolidate into a single Dockerfile
    Using ENTRYPOINT makes this sufficiently configurable

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-# This provisions a docker image from Ubuntu 12, installs required
-# tools, checks out repositories and runs all processes required to
-# build the documentation. It then launches a web server that serves
-# the documentation.
-
-FROM vohi/cfengine:doc-base
-MAINTAINER Volker Hilsheimer <volker.hilsheimer@gmail.com>
--- _setup/_rebuild.sh
+++ _setup/_rebuild.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-echo Rebuilding from branch "$1"
-
 RUN_SERVER=true
 RUN_JEKYLL=true
-BANCH=master
+REMOTE=cfengine
+BRANCH=master
 
 while [[ $# > 1 ]]
 do
@@ -17,6 +16,11 @@ do
       RUN_SERVER = false
       ;;
 
+    --remote|-r)
+      shift
+      REMOTE=$1
+      ;;
+
     *)
       BRANCH=$1
       ;;
@@ -26,15 +30,25 @@ do
 shift
 done
 
-# Update all clones, checkout branch
+echo Rebuilding from branch $BRANCH on remote $REMOTE...
+
+# Clone from remote, checkout branch
 cd $WRKDIR
-cd core && git checkout $BRANCH.x && git pull && make && cd ..
-cd masterfiles && git checkout $BRANCH.x && git pull && cd ..
-cd design-center && git checkout $BRANCH.x git pull && cd ..
-cd documentation && git checkout $BRANCH git pull && cd ..
-cd documentation-generator && git checkout $BRANCH && git pull
+
+git clone -b $BRANCH.x http://github.com/$REMOTE/masterfiles
+git clone -b $BRANCH.x http://github.com/$REMOTE/design-center
+git clone -b $BRANCH http://github.com/$REMOTE/documentation
+git clone -b $BRANCH http://github.com/$REMOTE/documentation-generator
+
+# build core - it's already checked out and built by base image
+cd core
+git checkout $BRANCH.x
+git pull
+make
+cd ..
 
 # Adjust jekyll config for docker file system layout
+cd documentation-generator
 sed -e "s/  display_path/# display_path/" -i _config.yml
 
 # build documentation
@@ -47,8 +61,10 @@ if $RUN_SERVER
 then
   jekyll --server 8080
 elif $RUN_JEKYLL
+then
   jekyll
 else
+  echo "Content cloned and pre-processed."
   echo "Run jekyll [--server 8080] to build [and serve] the HTML files"
   bash
 fi
