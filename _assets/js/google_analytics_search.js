c5927106223341cb2c2e8a23e434aa80ae073748
michaelclelland <michael.clelland@cfengine.com>
2015-12-07 09:59:20 +0100

    Update google_analytics_search.js

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,7 +1,7 @@
 //google search
 (function() {
     
-    var cx = '013522560145330013240:skcdq5u4lcc';
+    var cx = '013522560145330013240:dltrs-ywi9q';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
