02b55df51da8f2976018cff2ecf5dd847c617c4e
Nick Anderson <nick@cmdln.org>
2016-04-28 15:30:19 -0500

    Switch back to hard coded gcse
    Unfortunately, jekyll can not expand inside of js files.

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,7 +1,7 @@
 //google search
 (function() {
     
-    var cx = '{{site.cfengine.custom_search_engine}}';
+    var cx = '012826289311752768906:eahkid3txr8';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
