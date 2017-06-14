c6c681f5502822a787dec7025cf94402daac4b77
Nick Anderson <nick@cmdln.org>
2017-06-14 11:36:28 -0500

    Switch to 3.11 search results

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,7 +1,7 @@
 //google search
 (function() {
     
-    var cx = '012826289311752768906:ofqwltf6-kg';
+    var cx = '012826289311752768906:2riuky-1ece';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
