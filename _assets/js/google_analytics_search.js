94ce1f51af170e9ba6784fa4e3bf2548cea11bdd
Nick Anderson <nick@cmdln.org>
2018-05-08 17:31:31 -0500

    Update google custom search engine

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,15 +1,12 @@
 //google search
 (function() {
-    
-    var cx = '012826289311752768906:ofqwltf6-kg';
+    var cx = '012826289311752768906:jjhpbmruzem';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
-
 })();
 
 //google analytics
